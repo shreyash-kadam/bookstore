70b8949f7d14f32642cd58c0401f3601a802f3a8
shreyas-kadam <shreyash.s.kadam.02@gmail.com>
2025-12-03 11:42:23 +0530

    feat: add HomeController, index page, and temporary disable DB config

--- src/main/java/com/shreyashkadam/bookstore/BookstoreApplication.java
+++ src/main/java/com/shreyashkadam/bookstore/BookstoreApplication.java
@@ -2,12 +2,12 @@ package com.shreyashkadam.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
 }
