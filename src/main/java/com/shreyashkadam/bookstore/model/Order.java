f66c62fdfae19711add1de834caee721b8e994bc
shreyas-kadam <shreyash.s.kadam.02@gmail.com>
2025-12-07 10:01:31 +0530

    feat(order) : add order entry

---
+++ src/main/java/com/shreyashkadam/bookstore/model/Order.java
@@ -0,0 +1,22 @@
+package com.shreyashkadam.bookstore.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private double totalAmount;
+
+    private LocalDateTime orderDate;
+}
